dfff98ccb9a48742a99c49d4e3eb52a875787523
lsms <luis.masso@gft.com>
2021-10-05 16:03:51 +0200

    First commit

---
+++ CsvToJson_Converter/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace CsvToJson_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_convert = new System.Windows.Forms.Button();
+            this.txt_in = new System.Windows.Forms.TextBox();
+            this.txt_out = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_convert
+            // 
+            this.btn_convert.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_convert.Location = new System.Drawing.Point(539, 698);
+            this.btn_convert.Name = "btn_convert";
+            this.btn_convert.Size = new System.Drawing.Size(112, 44);
+            this.btn_convert.TabIndex = 0;
+            this.btn_convert.Text = "Convert";
+            this.btn_convert.UseVisualStyleBackColor = true;
+            this.btn_convert.Click += new System.EventHandler(this.btn_convert_Click);
+            // 
+            // txt_in
+            // 
+            this.txt_in.Location = new System.Drawing.Point(26, 56);
+            this.txt_in.Multiline = true;
+            this.txt_in.Name = "txt_in";
+            this.txt_in.Size = new System.Drawing.Size(585, 621);
+            this.txt_in.TabIndex = 1;
+            // 
+            // txt_out
+            // 
+            this.txt_out.Location = new System.Drawing.Point(702, 56);
+            this.txt_out.Multiline = true;
+            this.txt_out.Name = "txt_out";
+            this.txt_out.Size = new System.Drawing.Size(585, 621);
+            this.txt_out.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "CSV Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(702, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "JSON Text";
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Clear.Location = new System.Drawing.Point(667, 698);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(112, 44);
+            this.btn_Clear.TabIndex = 5;
+            this.btn_Clear.Text = "Clear";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1325, 754);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_out);
+            this.Controls.Add(this.txt_in);
+            this.Controls.Add(this.btn_convert);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_convert;
+        private System.Windows.Forms.TextBox txt_in;
+        private System.Windows.Forms.TextBox txt_out;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Clear;
+    }
+}
+
---
+++ CsvToJson_Converter/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Collections;
+
+namespace CsvToJson_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_convert_Click(object sender, EventArgs e)
+        {
+            if (txt_in.Text != "")
+                ConverCsvToJson();
+        }
+
+        /// <summary>
+        /// Main function of program
+        /// </summary>
+        private void ConverCsvToJson()
+        {
+            string line = "";
+            string jsonLine = "";
+
+            string lBracket = "[";
+            string rBracket = "]";
+            string lineBreak = "\r\n";
+
+            string[] columns;
+
+            List<String> csvList = new List<string>();
+            List<String> jsonList = new List<string>();
+
+            // Text area to List
+            txt_in.Text = txt_in.Text.Replace(";", ",");
+            csvList = txt_in.Lines.ToList();
+
+            // Extract columns
+            columns = SplitByComma(csvList[0]);
+
+            // Add first caracter of Json.
+            jsonList.Add(lBracket + lineBreak);
+
+            // Go over list of lines
+            for (int i = 1; i < csvList.Count; i++)
+            {
+
+                // Extract line
+                line = csvList[i].ToString();
+
+                // Convert line to Json line
+                jsonLine = ConverToJsonLine(columns, line);
+
+                if (i != csvList.Count - 1)
+                {
+                    // Take off last comma
+                    jsonLine = jsonLine + "," + lineBreak;
+                }
+
+                // Add Json Line to Json List
+                jsonList.Add(jsonLine);
+            }
+
+            // Add last caracter of Json.
+            jsonList.Add(lineBreak + rBracket);
+
+            // Json List to text area
+            txt_out.Text = "";
+            foreach (string elem in jsonList)
+            {
+                txt_out.Text += elem;
+            }
+        }
+
+        /// <summary>
+        /// Function to conver a csv line onto a Json object (an string with curly brackets and spaces or break lines)
+        /// </summary>
+        /// <param name="columns"></param>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        private string ConverToJsonLine(string[] columns, string line)
+        {
+            string[] jsonValues;
+            String jsonObject = "";
+
+            if (HasQuotes(line))
+            {
+                jsonValues = ExtractValuesWithQuotes(line);
+            }
+            else
+            {
+                jsonValues = ExtractValuesWithoutQuotes(line);
+            }
+
+            jsonObject = ValuesToJsonObject(columns, jsonValues);
+
+            return jsonObject;
+        }
+
+        /// <summary>
+        /// Funtion to extract values when line has not a composed value with commas
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        private string[] ExtractValuesWithoutQuotes(string line)
+        {
+            return SplitByComma(line);
+        }
+
+        /// <summary>
+        /// Funtion to extract values when line has a composed value with commas
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        private string[] ExtractValuesWithQuotes(string line)
+        {
+            string auxLine = line;
+            string[] auxArray;
+            ArrayList newArray = new ArrayList(); 
+            Queue queueLines = new Queue();
+
+            int i = 0;
+            foreach (Match match in Regex.Matches(line, "\"([^\"]*)\""))
+            {
+                auxLine = auxLine.Replace(match.ToString(), "Cell" + i);
+                queueLines.Enqueue(match.ToString());
+                i++;
+            }
+
+            auxArray = SplitByComma(auxLine.ToString());
+
+            int x = 0;
+            foreach(var elem in auxArray)
+            {
+                if (elem.Contains("Cell")) {
+                    newArray.Add(queueLines.Dequeue().ToString().Replace("\"", ""));
+                } else {
+                    newArray.Add(elem.ToString());
+                }
+
+            }
+
+            return (String[]) newArray.ToArray(typeof(string));
+        }
+
+        /// <summary>
+        /// Function to set up each json object between curly brackets 
+        /// </summary>
+        /// <param name="columns"></param>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        private string ValuesToJsonObject(string[] columns, string[] values)
+        {
+            string jsonProp = "";
+            string lCurly = "    {";
+            string rCurly = "    }";
+            string lineBreak = "\r\n";
+
+            jsonProp = lCurly + lineBreak;
+
+            for (int i = 0; i < columns.Length; i++)
+            {
+                jsonProp = jsonProp + ConcatColumnAndValue(columns[i].ToString(), values[i].ToString());
+
+                if (i == columns.Length - 1)
+                {
+                    jsonProp = TakeOffLastComma(jsonProp);
+                }
+
+                jsonProp = jsonProp + lineBreak;
+            }
+
+            jsonProp = TakeOffLastComma(jsonProp);
+
+
+            jsonProp = jsonProp + lineBreak + rCurly;
+
+            return jsonProp;
+        }
+
+        private string ConcatColumnAndValue(string col, string val)
+        {
+            if (IsNumeric(val))
+                return "        \"" + col.Trim() + "\"" + ": " + val + ",";
+                else
+                return "        \"" + col.Trim() + "\"" + ": " + "\"" + val + "\",";
+        }
+
+        /// <summary>
+        /// Function to know if line has va composed value with commas. 
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        private bool HasQuotes(string line)
+        {
+            return line.Contains('"');
+        }
+
+        /// <summary>
+        /// Funtión to take off the last comma in a line (when is final of jason object)
+        /// </summary>
+        /// <param name="jsonLine"></param>
+        /// <returns></returns>
+        private string TakeOffLastComma(string jsonLine)
+        {
+            return jsonLine.Substring(0, jsonLine.Length - 1);
+        }
+
+        /// <summary>
+        /// Function to split values from a line
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        private string[] SplitByComma(string line)
+        {
+            return line.Split(',');
+        }
+     
+        /// <summary>
+        /// Procedure to clear text areas
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            txt_in.Text = "";
+            txt_out.Text = "";
+        }
+
+        /// <summary>
+        /// Functio to get if string could be a number
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool IsNumeric(string value)
+        {
+            bool result = int.TryParse(value, out int i) || double.TryParse(value, out double x);
+            return result;
+        }
+    }
+}
